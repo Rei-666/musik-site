7a1a2480e7d19119bee5e834d774c4db9e95cec2
Rei-666 <wszyzdgo@gmail.com>
2020-07-08 13:25:37 +0200

    Repair responsivness on tablet devices

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
 
+
 function matchWidth(x){
   var btn = document.getElementById("candidat__actionbutton");
 
   if(x.matches) {
     var form = document.getElementById("form");
     form.appendChild(btn);
-    console.log("aaaa")
   }
   else {
     var candidat = document.getElementById("candidat");
     candidat.appendChild(btn); 
-    console.log("o chuj ci chodzi");
+    btn.addEventListener("click", showForm);
   }
 }
 
+var btn = document.getElementById("candidat__actionbutton");
 
 var x = window.matchMedia("(min-width: 768px)");
 matchWidth(x);
 x.addListener(matchWidth);
+
+function showForm(){
+  var form = document.getElementById("form");
+  form.classList.remove("hidden");
+  form.classList.add("shown");
+}
+function hideForm(){
+  var form = document.getElementById("form");
+  form.classList.remove("shown");
+  form.classList.add("hidden");
+}
