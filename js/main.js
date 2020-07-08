1a9d144d4be9f33930128250647361a3259f0e40
Rei-666 <wszyzdgo@gmail.com>
2020-07-08 17:51:54 +0200

    Add mobile view of form and animation of opening this form

--- js/main.js
+++ js/main.js
@@ -1,32 +1,47 @@
 
 
+const btn = document.getElementById("candidat__actionbutton");
+const x_btn = document.getElementById("form__x-button");
+x_btn.addEventListener("click", hideForm);
+const x = window.matchMedia("(min-width: 768px)");
+matchWidth(x);
+x.addListener(matchWidth);
+
+function formAction(){
+  if(window.matchMedia("(min-width: 768px)").matches){
+    document.getElementById("candidat__actionbutton").innerHTML = "pieseczek";
+  }
+  else
+  {
+  const form = document.getElementById("form");
+  document.body.classList.add("body--noscroll");
+  form.classList.remove("form--hidden", "slideDown");
+  form.classList.add("form--shown", "slideDownReturn");
+  }
+}
+
+function hideForm(){
+  const form = document.getElementById("form");
+  document.body.classList.remove("body--noscroll");
+  form.classList.remove("form--shown", "slideDownReturn");
+  form.classList.add("slideDown");
+  setTimeout(() => {
+    form.classList.add("form--hidden");
+  }, 1000);
+}
+
+
 function matchWidth(x){
-  var btn = document.getElementById("candidat__actionbutton");
+  const btn = document.getElementById("candidat__actionbutton");
 
   if(x.matches) {
     var form = document.getElementById("form");
     form.appendChild(btn);
+    form.classList.remove("slideDown");
   }
   else {
     var candidat = document.getElementById("candidat");
     candidat.appendChild(btn); 
-    btn.addEventListener("click", showForm);
+    btn.addEventListener("click", formAction);
   }
-}
-
-var btn = document.getElementById("candidat__actionbutton");
-
-var x = window.matchMedia("(min-width: 768px)");
-matchWidth(x);
-x.addListener(matchWidth);
-
-function showForm(){
-  var form = document.getElementById("form");
-  form.classList.remove("hidden");
-  form.classList.add("shown");
-}
-function hideForm(){
-  var form = document.getElementById("form");
-  form.classList.remove("shown");
-  form.classList.add("hidden");
 }
