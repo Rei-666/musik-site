c67840e9cf88af944a3f1806991dea2ccc77ef84
Rei-666 <wszyzdgo@gmail.com>
2020-07-11 21:53:05 +0200

    Add styling to form inputs and email validation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 
 
-const btn = document.getElementById("candidat__actionbutton");
+const btn = document.getElementById("button");
 const x_btn = document.getElementById("form__x-button");
+const email_input = document.getElementById("email");
 x_btn.addEventListener("click", hideForm);
+email_input.addEventListener("focusout", validateEmail);
 const x = window.matchMedia("(min-width: 768px)");
 matchWidth(x);
 x.addListener(matchWidth);
 
 function formAction(){
   if(window.matchMedia("(min-width: 768px)").matches){
-    document.getElementById("candidat__actionbutton").innerHTML = "pieseczek";
+    document.getElementById("button").innerHTML = "pieseczek";
   }
   else
   {
@@ -30,9 +32,21 @@ function hideForm(){
   }, 1000);
 }
 
+function validateEmail(){
+  const email_input = document.getElementById("email");
+  const email_label = document.getElementById("email_label");
+  var mail_ver = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(!mail_ver.test(email_input.value.toLowerCase())){
+    email_label.classList.add("email--error");
+    }
+    else{
+      email_label.classList.remove("email--error");
+      console.log("pieseczek i: " + email_input.value.toLowerCase());
+    }
+}
 
 function matchWidth(x){
-  const btn = document.getElementById("candidat__actionbutton");
+  const btn = document.getElementById("button");
 
   if(x.matches) {
     var form = document.getElementById("form");
