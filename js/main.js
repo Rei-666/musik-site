811d24822bf7e1c569f67e9dd9254e12e444d21d
Rei-666 <wszyzdgo@gmail.com>
2020-08-14 13:14:16 +0200

    Add confirmed_email.html site

--- js/main.js
+++ js/main.js
@@ -1,11 +1,56 @@
 const btn = document.getElementById("button");
+
 const x_btn = document.getElementById("form__x-button");
-const email_input = document.getElementById("email");
 x_btn.addEventListener("click", hideForm);
+
+const form = document.getElementById("form");
+
+const form_button = document.getElementById("button--form")
+form_button.addEventListener("click", onSubmit)
+
+const name_input = document.getElementById("name")
+const surname_input = document.getElementById("surname")
+const email_input = document.getElementById("email");
 email_input.addEventListener("focusout", validateEmail);
+
 const x = window.matchMedia("(min-width: 768px)");
 matchWidth(x);
 x.addListener(matchWidth);
+
+const recaptcha_key='6LeWR7sZAAAAAE1f7xS_gfuHpR2GuPwsLnhrqB8C'
+const recaptcha_url='http://192.168.0.100:5000/add_email'
+
+function formSubmit(url, response, redirect, body){
+
+  fetch(url+"?response="+response+"&redirect="+redirect, {
+  method: 'POST',
+  body: body
+  })
+  .then(response => response.json())
+  .then(data => {
+  console.log('Success:', data);
+  })
+  .catch((error) => {
+  console.error('Error:' + error);
+});
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  grecaptcha.ready(
+    function() {
+    grecaptcha.execute(recaptcha_key, {action: 'submit'}).then(
+      function(token){
+      var formData = new FormData();
+      redirect = window.location.protocol + "//" + window.location.hostname + "/confirm_email.html";
+      formData.append("email", email_input.value);
+      formData.append("name", name_input.value);
+      formData.append("surname", surname_input.value);
+      formSubmit(recaptcha_url, token, redirect, formData);
+      })
+    })
+  }
+
 
 function formAction(){
   if(window.matchMedia("(min-width: 768px)").matches){
@@ -13,7 +58,6 @@ function formAction(){
   }
   else
   {
-  const form = document.getElementById("form");
   document.body.classList.add("body--noscroll");
   form.classList.remove("form--hidden", "slideDown");
   form.classList.add("form--shown", "slideDownReturn");
@@ -21,7 +65,6 @@ function formAction(){
 }
 
 function hideForm(){
-  const form = document.getElementById("form");
   document.body.classList.remove("body--noscroll");
   form.classList.remove("form--shown", "slideDownReturn");
   form.classList.add("slideDown");
@@ -31,7 +74,6 @@ function hideForm(){
 }
 
 function validateEmail(){
-  const email_input = document.getElementById("email");
   const email_label = document.getElementById("email_label");
   var mail_ver = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if(!mail_ver.test(email_input.value.toLowerCase())){
@@ -47,7 +89,6 @@ function matchWidth(x){
   const btn = document.getElementById("button");
 
   if(x.matches) {
-    var form = document.getElementById("form");
     form.appendChild(btn);
     form.classList.remove("slideDown");
   }
